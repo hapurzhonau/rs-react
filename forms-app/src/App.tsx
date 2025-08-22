0f2dd81197957174d83671cc0495c868f38334a6
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2025-08-22 14:59:36 +0300

    chore: add file structure

--- forms-app/src/App.tsx
+++ forms-app/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <p className="text-pink-400">forms</p>;
+  return <p className="text-pink-400 border-2 rounded-md p-2">forms</p>;
 }
 
 export default App;
