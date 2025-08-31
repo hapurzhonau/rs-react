70d1b31d699690df054c1ae53d13dd3608eddb33
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2025-08-31 13:10:05 +0300

    feat: change population corresponds of year

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,11 +1,13 @@
-import { use, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import { getData } from '../../get-data/get-data';
 import Container from '../container/container';
 import CountryContainer from '../container/country-container';
 import { Modal } from '../modal/modal';
 import ColumnSelector from '../column-selector/column-selector';
+import ButtonSelect from '../button-select/button-select';
+import { YearSelector } from '../year-selector/year-selector';
 
-interface IYearRow {
+export interface IYearRow {
   year: number;
   population?: number | null;
   co2?: number;
@@ -31,27 +33,48 @@ export default function List() {
   const [openCountry, setOpenCountry] = useState<string | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedColumns, setSelectedColumns] = useState<string[]>([]);
+  const [selectedYear, setSelectedYear] = useState<number | null>(null);
 
-  const handleOpen = (name: string) => {
+  const years = countries[0]?.[1].data.map((row) => row.year) ?? [];
+
+  const handleOpenCountry = (name: string) => {
     setOpenCountry((prev) => (prev === name ? null : name));
   };
 
+  useEffect(() => {
+    if (years.length > 0 && selectedYear === null) {
+      const lastYear = years.at(-1);
+      if (lastYear) setSelectedYear(lastYear);
+    }
+  }, [years, selectedYear]);
+
   return (
     <div className="flex max-w-fit mx-auto py-6 flex-col gap-2">
-      <button
-        onClick={() => setIsModalOpen(true)}
-        className="border rounded-md max-w-fit px-2 cursor-pointer bg-gray-700 hover:bg-gray-900"
-      >
-        Select columns
-      </button>
+      <header className="flex gap-20 sticky top-0 p-4 bg-gray-800/85 border-b-4 border-gray-700">
+        <ButtonSelect
+          onClick={() => {
+            setIsModalOpen(true);
+          }}
+        >
+          Select columns
+        </ButtonSelect>
+        <YearSelector
+          onChange={setSelectedYear}
+          years={years}
+          selectedYear={selectedYear ?? 0}
+        />
+      </header>
+
       <ul className="flex flex-col gap-2">
         {countries.map(([name, node]) => {
-          const latest = node.data.at(-1);
+          const rowForSelectedYear = node.data.find(
+            (el) => el.year === selectedYear
+          );
           return (
             <li
               key={name}
               className="border-1 cursor-pointer border-gray-700"
-              onClick={() => handleOpen(name)}
+              onClick={() => handleOpenCountry(name)}
             >
               <div className="flex justify-center">
                 <Container className="min-w-[40vw] border-r-1">
@@ -60,11 +83,15 @@ export default function List() {
                 <Container className="border-r-1">
                   {node.iso_code ?? 'N/A'}
                 </Container>
-                <Container>{latest?.population ?? 'N/A'}</Container>
+                <Container>
+                  {rowForSelectedYear
+                    ? (rowForSelectedYear.population ?? 'N/A')
+                    : 'N/A'}
+                </Container>
               </div>
 
               {openCountry === name && (
-                <ul className="max-w-11/12 mx-auto p-2 flex flex-col gap-1 bg-gray-700 text-pink-200 border border-pink-400 rounded-md shadow-black shadow-2xl cursor-auto mt-2">
+                <ul className="max-w-11/12 mx-auto p-2 flex flex-col gap-1 bg-gray-700 text-pink-200 border-2 border-pink-400 rounded-md shadow-black shadow-2xl cursor-auto mt-2">
                   <div className="flex font-bold border-b border-pink-300 pb-1 mb-1">
                     <CountryContainer>Year</CountryContainer>
                     <CountryContainer>Population</CountryContainer>
